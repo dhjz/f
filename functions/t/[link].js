699c390abcc3cffb0affc1c4339b85345463d14d
dinghj3 <dinghj3@asiainfo.com>
2024-10-12 10:13:31 +0800

    link

--- functions/t/[link].js
+++ functions/t/[link].js
@@ -1,6 +1,7 @@
 export const onRequest = async (ctx) => {
-  const { request, params } = ctx
+  const { request, params = {} } = ctx
   const task = await ctx.env.dhjz.get("dhjz");
-console.log(task, request.headers);
+  await ctx.env.dhjz.put("test", new Date().toLocaleString());
+  console.log(task, request.headers);
   return new Response(`${task}::${request.url}暂未到获取ip${JSON.stringify(params)}`);
 };
