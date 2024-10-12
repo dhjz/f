2df6f33819abfd68ea15a308cb22a11ddd6e930e
dinghj3 <dinghj3@asiainfo.com>
2024-10-12 11:43:50 +0800

    1

--- functions/t/index.js
+++ functions/t/index.js
@@ -6,11 +6,11 @@ export const onRequest = async ({ request, env }) => {
     const list = await env.dhjz.list();
     return new Response(JSON.stringify(list));
   } else if (type == 'get' && key) {
-    const val = await env.dhjz.get(key);
+    const val = await env.dhjz.get('link:' + key);
     return new Response(val);
   } else if (type == 'put' && key && val) {
-    await env.dhjz.put(key, val);
-    return new Response(true);
+    await env.dhjz.put('link:' + key, val);
+    return new Response('添加成功:' + key + '|' + val);
   }
   
   return new Response(`请传入正确的参数type, key, val`);
