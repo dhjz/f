436a6f37df20ca4326ef7aead1cbf52f8cbebd0b
dinghj3 <dinghj3@asiainfo.com>
2024-10-12 10:06:40 +0800

    测试

--- functions/t.js
+++ functions/t.js
@@ -2,5 +2,5 @@ export const onRequest = async (ctx) => {
   const { request, params } = ctx
   const task = await ctx.env.dhjz.get("dhjz");
 console.log(task, request.headers);
-  return new Response((task + '暂未到获取ip' + JSON.stringify(request)));
+  return new Response(`${task}::${request.url}暂未到获取ip${JSON.stringify(params)}`);
 };
