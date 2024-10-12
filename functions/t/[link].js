928b91f79c494f403368b164f665b6fb606491b1
dinghj3 <dinghj3@asiainfo.com>
2024-10-12 11:04:07 +0800

    锻炼

--- functions/t/[link].js
+++ functions/t/[link].js
@@ -1,9 +1,9 @@
 export const onRequest = async (ctx) => {
   const { request, params = {} } = ctx
   if (params.link) {
-    const link = await ctx.env.dhjz.get(params.link);
+    const link = await ctx.env.dhjz.get('link:' + params.link);
     if (link) {
-      return Response.redirect(link)
+      return Response.redirect(decodeURIComponent(link))
     }
   }
   return new Response(`该短链不存在或已失效`);
