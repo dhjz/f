e0b57be2f1d37ed56b43532b3d2dfe3977d8a894
dinghj3 <dinghj3@asiainfo.com>
2024-10-21 17:05:34 +0800

    q

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -4,6 +4,9 @@
 export const onRequest = async (context) => {
   const response = await context.next();
   response.headers.set('Access-Control-Allow-Origin', '*');
+  response.headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  response.headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  response.headers.set("Access-Control-Allow-Credentials", "true");
   response.headers.set('Access-Control-Max-Age', '86400');
   return response;
 };
