34a7f9de00edd843b5c21b5d69aef63c55e37ecb
dinghj3 <dinghj3@asiainfo.com>
2024-10-21 16:46:59 +0800

    测试post

--- functions/t/index.js
+++ functions/t/index.js
@@ -1,6 +1,12 @@
-export const onRequest = async ({ request, env }) => {
+export const onRequest = async ({ request, env, data }) => {
   const params = Object.fromEntries(new URL(request.url).searchParams) || {};
   const { type, key, val } = params
+  let jsonStr = '666'
+  if (request.method.toLowerCase() === 'post') {
+    try {
+      jsonStr = await readStreamAsJson(request.body)
+    } catch (e) {}
+  }
   // response.headers.set('Content-Type', 'application/json')
   if (type == 'list') {
     const list = await env.dhjz.list();
@@ -19,5 +25,34 @@ export const onRequest = async ({ request, env }) => {
     return new Response('添加成功, ' + key + '|' + val);
   }
   
-  return new Response(`请传入正确的参数type, key, val`);
+  return new Response(`请传入正确的参数type, key, val` + JSON.stringify(data) + '|||' + jsonStr);
 };
+
+async function readStreamAsJson(stream) {
+  const chunks = [];
+
+  return new Promise((resolve, reject) => {
+      stream.on('data', chunk => {
+          chunks.push(chunk);
+      });
+
+      stream.on('end', () => {
+          // 将 Buffer 数组连接成一个完整的 Buffer
+          const buffer = Buffer.concat(chunks);
+          // 将 Buffer 转换为字符串
+          const jsonString = buffer.toString();
+          // 解析为 JSON 对象
+          try {
+              const jsonData = JSON.parse(jsonString);
+              resolve(jsonData);
+          } catch (error) {
+              resolve(jsonString)
+              // reject(error);
+          }
+      });
+
+      stream.on('error', err => {
+          reject(err);
+      });
+  });
+}
