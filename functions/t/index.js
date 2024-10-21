6eeebebd0bfdfb74771927933978751ebdbed90d
dinghj3 <dinghj3@asiainfo.com>
2024-10-21 16:53:24 +0800

    ces

--- functions/t/index.js
+++ functions/t/index.js
@@ -2,9 +2,15 @@ export const onRequest = async ({ request, env, data }) => {
   const params = Object.fromEntries(new URL(request.url).searchParams) || {};
   const { type, key, val } = params
   let jsonStr = '666'
+  let jsonStr1 = '777'
   if (request.method.toLowerCase() === 'post') {
     try {
       jsonStr = await readStreamAsJson(request.body)
+    } catch (e) {}
+  }
+  if (request.method.toLowerCase() === 'post') {
+    try {
+      jsonStr1 = await parseReqData(request)
     } catch (e) {}
   }
   // response.headers.set('Content-Type', 'application/json')
@@ -25,7 +31,7 @@ export const onRequest = async ({ request, env, data }) => {
     return new Response('添加成功, ' + key + '|' + val);
   }
   
-  return new Response(`请传入正确的参数type, key, val` + JSON.stringify(data) + '|||' + jsonStr);
+  return new Response(`请传入正确的参数type, key, val` + JSON.stringify(data) + '|||' + jsonStr + '|||' + jsonStr1);
 };
 
 async function readStreamAsJson(stream) {
@@ -55,4 +61,30 @@ async function readStreamAsJson(stream) {
           reject(err);
       });
   });
+}
+
+
+async function parseReqData(request) {
+  const { headers } = request;
+  if (request.method === "GET") {
+      return Object.fromEntries(new URL(request.url).searchParams)
+  } else {//request method = post
+      const contentType = headers.get('content-type') || '';
+      if (contentType.includes('application/json')) {
+          return await request.json();
+      } else if (contentType.includes('application/text')) {
+        return request.text();
+      } else if (contentType.includes('text/html')) {
+        return request.text();
+      } else if (contentType.includes('form')) {
+          const formData = await request.formData();
+          const body = {};
+          for (const entry of formData.entries()) {
+              body[entry[0]] = entry[1];
+          }
+          return body;
+      } else {
+          return '';
+      }
+  }
 }
