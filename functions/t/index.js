697b67c6d6e3ef2387e8c0eff65c97a70a2e2ed0
dhjz <909514844@qq.com>
2024-10-27 12:53:27 +0800

    1

--- functions/t/index.js
+++ functions/t/index.js
@@ -33,13 +33,13 @@ export const onRequest = async ({ request, env, next }) => {
     return new Response(val);
   } else if (type == 'putlink' && key && val) {
     await env.dhjz.put('link:' + key, val, options);
-    return new Response('添加成功, link:' + key + '|' + val);
+    return new Response('添加成功, link:' + key + '|' + (val.length > 1000 ? '' : val));
   } else if (type == 'get' && key) {
     const val = await env.dhjz.get(key);
     return new Response(val);
   } else if (type == 'put' && key && val) {
     await env.dhjz.put(key, val, options);
-    return new Response('添加成功, ' + key + '|' + val);
+    return new Response('添加成功, ' + key + '|' + (val.length > 1000 ? '' : val));
   }
   
   return new Response(`请传入正确的参数type, key, val, ${JSON.stringify(jsonStr1)}`);
